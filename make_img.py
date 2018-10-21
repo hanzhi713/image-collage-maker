ec6245655e2a217335aaac1ac2cf0a9393d388d1
Hanzhi Zhou <hanzhi713@163.com>
2018-10-21 00:55:32 -0400

    Updated sort methods
    Former-commit-id: cf9565da07d20fcc0991caf8a2f68713f634529f
    Former-commit-id: 6b07563483d207ca59a9d388e863b42198aea3d0

--- make_img.py
+++ make_img.py
@@ -10,6 +10,14 @@ import random
 from tqdm import tqdm
 
 
+def bgr(img):
+    return np.mean(img[:, :, 0]), np.mean(img[:, :, 1]), np.mean(img[:, :, 2])
+
+
+def bgr_chl_sum(img):
+    return np.sum(img[:, :, 0]), np.sum(img[:, :, 1]), np.sum(img[:, :, 2])
+
+
 def bgr_sum(img):
     return np.sum(img)
 
@@ -21,12 +29,16 @@ def av_hue(img):
 
 def hsv(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = np.mean(hsv[:, :, 0]), np.mean(hsv[:, :, 1]), np.mean(hsv[:, :, 2])
-    return h, s, v
+    return np.mean(hsv[:, :, 0]), np.mean(hsv[:, :, 1]), np.mean(hsv[:, :, 2])
 
 
 def lum(img):
     return np.mean(np.sqrt(0.241 * img[:, :, 0] + .691 * img[:, :, 1] + .068 * img[:, :, 2]))
+
+
+def lab(img):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    return np.mean(lab[:, :, 0]), np.mean(lab[:, :, 1]), np.mean(lab[:, :, 2])
 
 
 def rand(img):
@@ -35,28 +47,32 @@ def rand(img):
 
 if __name__ == "__main__":
     # 获取下载的所有图片
-    pic_path = 'img'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pic_path", help="Path to the downloaded head images", default="img", type=str)
+    parser.add_argument("--size", help="Size of each image in pixels", type=int, default=100)
+    parser.add_argument("--ratio", help="Aspect ratio", nargs='+', type=int, default=[16, 9])
+    parser.add_argument("--sort", help="Sort method",
+                        choices=["bgr", "bgr_sum", "av_hue", "lum", "lab", "rand", "pca_bgr", "pca_hsv",
+                                 "pca_lab"],
+                        type=str, default="bgr_sum")
+    parser.add_argument("--rev_row", help="Whether to use the S-shaped alignment", type=bool, default=True)
+
+    args = parser.parse_args()
+
+    # aspect ratio
+    ratio = tuple(args.ratio)
+    rw, rh = ratio
+
+    pic_path = args.pic_path
     files = list(os.walk(pic_path))[0][-1]
     try:
         files.remove("cache.pkl")
     except:
         pass
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--size", help="Size of each image", type=int, default=100)
-    parser.add_argument("--ratio", help="Aspect ratio", nargs='+', type=int, default=[16, 9])
-    parser.add_argument("--sort", help="Sort method.", choices=["bgr_sum", "av_hue", "hsv", "lum", "rand"],
-                        type=str, default="bgr_sum")
-
-    args = parser.parse_args()
-
-    # aspect ratio
-    ratio = tuple(args.ratio)
-    sort_key = eval(args.sort)
-    rw, rh = ratio
-
     num_friends = len(files)
     img_size = args.size, args.size
+    rev = args.rev_row
 
     possible_wh = []
     print("Calculating grid size...")
@@ -72,26 +88,53 @@ if __name__ == "__main__":
     result_grid = best_wh
 
     sorted_imgs = []
-    for i, img_file in tqdm(enumerate(files), desc="[Reading images]", unit="imgs"):
-        try:
-            img = cv2.resize(cv2.imread(os.path.join(pic_path, img_file)), img_size, interpolation=cv2.INTER_CUBIC)
-        except:
-            # 如果读不出来图像，就用黑色块代替
-            img = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+    if args.sort.startswith("pca_"):
+        sort_key = eval(args.sort[args.sort.find('_') + 1:])
+        from sklearn.decomposition import PCA
 
-        sorted_imgs.append((img, sort_key(img)))
+        pca = PCA(1)
+        imgs = []
+        for i, img_file in tqdm(enumerate(files), total=len(files), desc="[Reading images]", unit="imgs"):
+            try:
+                img = cv2.resize(cv2.imread(os.path.join(pic_path, img_file)), img_size, interpolation=cv2.INTER_CUBIC)
+            except:
+                img = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+            sorted_imgs.append(sort_key(img))
+            imgs.append(img)
 
-    sorted_imgs.sort(key=lambda x: x[1])
-    sorted_imgs = [a for a, b in sorted_imgs]
+        one_d_repr = pca.fit_transform(sorted_imgs)[:, 0]
+        sorted_imgs = np.array(imgs)[np.argsort(one_d_repr)]
+
+    else:
+        sort_key = eval(args.sort)
+
+        for i, img_file in tqdm(enumerate(files), total=len(files), desc="[Reading images]", unit="imgs"):
+            try:
+                img = cv2.resize(cv2.imread(os.path.join(pic_path, img_file)), img_size, interpolation=cv2.INTER_CUBIC)
+            except:
+                img = np.zeros((img_size[0], img_size[1], 3), dtype=np.uint8)
+            sorted_imgs.append((img, sort_key(img)))
+
+        sorted_imgs.sort(key=lambda x: x[1])
+        sorted_imgs = [a for a, b in sorted_imgs]
 
     for i in tqdm(range(result_grid[1]), desc="[Processing]", unit="imgs"):
         img_f = sorted_imgs[i * result_grid[0]]
-        for j in range(1, result_grid[0]):
-            img_t = sorted_imgs[i * result_grid[0] + j]
-            img_f = np.append(img_f, img_t, axis=1)
-        if i == 0:
-            combined_img = img_f
+        if rev and i % 2 == 1:
+            for j in range(result_grid[0] - 1, 0, -1):
+                img_t = sorted_imgs[i * result_grid[0] + j]
+                img_f = np.append(img_f, img_t, axis=1)
+            if i == 0:
+                combined_img = img_f
+            else:
+                combined_img = np.append(combined_img, img_f, axis=0)
         else:
-            combined_img = np.append(combined_img, img_f, axis=0)
+            for j in range(1, result_grid[0]):
+                img_t = sorted_imgs[i * result_grid[0] + j]
+                img_f = np.append(img_f, img_t, axis=1)
+            if i == 0:
+                combined_img = img_f
+            else:
+                combined_img = np.append(combined_img, img_f, axis=0)
 
-    cv2.imwrite('result.png', combined_img)
+    cv2.imwrite('result-{}.png'.format(args.sort), combined_img)
