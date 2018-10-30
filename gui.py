21b01e994e1bb5ec6c62be5ef75017687e71fd42
hanzhi713 <hanzhi713@163.com>
2018-10-30 09:30:44 -0400

    GUI Improved
    Former-commit-id: addc7ca4e86074fd82f918a72f4404bfc4515023
    Former-commit-id: dc642e522eb0baa9ecdf11e71991474d2787f3f0 [formerly 852eb912fb6ca4351c376eeaf96a10f5d67d46fb]
    Former-commit-id: 0e61cd1dd4f4fb6f7aafeaa3b3217eb202ee6e15

--- gui.py
+++ gui.py
@@ -45,34 +45,31 @@ class SafeText(Text):
     def __init__(self, master, **options):
         Text.__init__(self, master, **options)
         self.queue = Queue()
-        self.clear_line = False
-        self.last_line_len = 1
         self.update_me()
 
-    def write(self, line):
+    def write(self, line: str):
         self.queue.put(line)
 
     def flush(self):
-        self.queue.put(None)
+        pass
 
+    # this one run in the main thread
     def update_me(self):
         try:
             while True:
                 line = self.queue.get_nowait()
-                if line is None:
-                    self.clear_line = True
-                else:
-                    # if self.queue.empty():
-                    #     self.clear_line = False
-                    if self.clear_line:
-                        self.delete("end-{}c".format(self.last_line_len), END)
-                        self.clear_line = False
 
-                    current_line = str(line)
-                    self.insert(END, current_line)
-                    self.last_line_len = len(current_line)
-                    self.see("end-1c")
-                    self.update_idletasks()
+                # a naive way to process the \r control char
+                if line.find("\r") > -1:
+                    line = line.replace("\r", "")
+                    row = int(self.index(END).split(".")[0])
+                    self.delete("{}.0".format(row - 1),
+                                "{}.{}".format(row - 1, len(line)))
+                    self.insert("end-1c linestart", line)
+                else:
+                    self.insert(END, line)
+                self.see("end-1c")
+                self.update_idletasks()
         except:
             pass
         self.after(50, self.update_me)
@@ -91,7 +88,7 @@ class OutputWrapper:
         self.cmd_log = cmd_log
         self.encoding = "utf-8"
 
-    def write(self, s):
+    def write(self, s: str):
         self.cmd_log.write(s)
 
     def flush(self):
@@ -112,7 +109,8 @@ def show_img(img):
                                       cv2.INTER_CUBIC), cv2.COLOR_BGR2RGB)
     root.preview = ImageTk.PhotoImage(image=Image.fromarray(preview))
     canvas.delete("all")
-    canvas.create_image((800 - w) // 2, (500 - h) // 2, image=root.preview, anchor=NW)
+    canvas.create_image((800 - w) // 2, (500 - h) // 2,
+                        image=root.preview, anchor=NW)
     print("Done", file=out_wrapper)
 
 
@@ -123,15 +121,18 @@ right_top_panel.grid(row=0, column=0, pady=10, sticky="W")
 
 file_path = StringVar()
 file_path.set("N/A")
-Label(right_top_panel, text="Path of source images:").grid(columnspan=2, sticky="W")
-Label(right_top_panel, textvariable=file_path, wraplength=150).grid(row=1, columnspan=2, sticky="W")
+Label(right_top_panel, text="Path of source images:").grid(
+    columnspan=2, sticky="W")
+Label(right_top_panel, textvariable=file_path, wraplength=150).grid(
+    row=1, columnspan=2, sticky="W")
 
 opt = StringVar()
 opt.set("sort")
 img_size = IntVar()
 img_size.set(50)
 Label(right_top_panel, text="Image size: ").grid(row=2, column=0, pady=5)
-Entry(right_top_panel, width=5, textvariable=img_size).grid(row=2, column=1, sticky="W")
+Entry(right_top_panel, width=5, textvariable=img_size).grid(
+    row=2, column=1, sticky="W")
 
 imgs = None
 current_image = None
@@ -139,7 +140,8 @@ current_image = None
 
 def load_images():
     global imgs, current_image
-    fp = filedialog.askdirectory(initialdir=os.path.dirname(__file__), title="Select folder")
+    fp = filedialog.askdirectory(
+        initialdir=os.path.dirname(__file__), title="Select folder")
     if fp is not None and len(fp) >= 0 and os.path.isdir(fp):
         file_path.set(fp)
     else:
@@ -165,19 +167,24 @@ def load_images():
         messagebox.showerror("Error", t)
 
 
-Button(right_top_panel, text=" Load images ", command=load_images).grid(row=3, column=0, columnspan=2, pady=(0, 5))
+Button(right_top_panel, text=" Load images ", command=load_images).grid(
+    row=3, column=0, columnspan=2, pady=(0, 5))
 
-Separator(right_panel, orient="horizontal").grid(row=1, columnspan=2, sticky="we")
+Separator(right_panel, orient="horizontal").grid(
+    row=1, columnspan=2, sticky="we")
 
 right_sort_opt_panel = PanedWindow(right_panel)
 right_sort_opt_panel.grid(row=2, column=0, pady=10, sticky="W")
 
 sort_method = StringVar()
 sort_method.set("pca_lab")
-Label(right_sort_opt_panel, text="Sort methods:").grid(row=0, column=0, pady=5, sticky="W")
-OptionMenu(right_sort_opt_panel, sort_method, "", *make_img.all_sort_methods).grid(row=0, column=1)
+Label(right_sort_opt_panel, text="Sort methods:").grid(
+    row=0, column=0, pady=5, sticky="W")
+OptionMenu(right_sort_opt_panel, sort_method, "", *
+           make_img.all_sort_methods).grid(row=0, column=1)
 
-Label(right_sort_opt_panel, text="Aspect ratio:").grid(row=1, column=0, sticky="W")
+Label(right_sort_opt_panel, text="Aspect ratio:").grid(
+    row=1, column=0, sticky="W")
 aspect_ratio_panel = PanedWindow(right_sort_opt_panel)
 rw = IntVar()
 rw.set(16)
@@ -190,17 +197,22 @@ Entry(aspect_ratio_panel, width=3, textvariable=rh).grid(row=0, column=2)
 
 rev_row = IntVar()
 rev_sort = IntVar()
-Checkbutton(right_sort_opt_panel, variable=rev_row, text="Reverse consecutive row").grid(row=2, columnspan=2,
-                                                                                         sticky="W")
-Checkbutton(right_sort_opt_panel, variable=rev_sort, text="Reverse sort direction").grid(row=3, columnspan=2,
-                                                                                         sticky="W")
+Checkbutton(right_sort_opt_panel, variable=rev_row,
+            text="Reverse consecutive row").grid(row=2, columnspan=2, sticky="W")
+Checkbutton(right_sort_opt_panel, variable=rev_sort,
+            text="Reverse sort direction").grid(row=3, columnspan=2, sticky="W")
 
 
 def generate_sorted_image():
     if imgs is None:
         messagebox.showerror("Empty set", "Please first load images")
     else:
-        w, h = rw.get(), rh.get()
+        try:
+            w, h = rw.get(), rh.get()
+            assert w > 0, "Width must be greater than 0"
+            assert h > 0, "Height must be greater than 0"
+        except:
+            return messagebox.showerror("Illegal Argument", traceback.format_exc())
 
         def action():
             result_grid, sorted_imgs = make_img.sort_collage(imgs, (w, h), sort_method.get(), rev_sort.get(),
@@ -222,8 +234,10 @@ color_space.set("lab")
 dest_img_path = StringVar()
 dest_img_path.set("N/A")
 dest_img = None
-Label(right_collage_opt_panel, text="Path of destination image").grid(row=0, columnspan=2, sticky="W")
-Label(right_collage_opt_panel, textvariable=dest_img_path, wraplength=150).grid(row=1, columnspan=2, sticky="W")
+Label(right_collage_opt_panel, text="Path of destination image").grid(
+    row=0, columnspan=2, sticky="W")
+Label(right_collage_opt_panel, textvariable=dest_img_path,
+      wraplength=150).grid(row=1, columnspan=2, sticky="W")
 
 
 def load_dest_img():
@@ -241,20 +255,25 @@ def load_dest_img():
                 show_img(dest_img)
                 dest_img_path.set(fp)
             except:
-                messagebox.showerror("Error reading file", traceback.format_exc())
+                messagebox.showerror("Error reading file",
+                                     traceback.format_exc())
         else:
             return
 
 
 Button(right_collage_opt_panel, text="Load destination image",
        command=load_dest_img).grid(row=2, columnspan=2)
-Label(right_collage_opt_panel, text="Sigma: ").grid(row=3, column=0, sticky="W", pady=(10, 2))
-Entry(right_collage_opt_panel, textvariable=sigma, width=8).grid(row=3, column=1, sticky="W", pady=(10, 2))
-Label(right_collage_opt_panel, text="Color space: ").grid(row=4, column=0, sticky="W")
-OptionMenu(right_collage_opt_panel, color_space, "",
-           *make_img.all_color_spaces).grid(row=4, column=1, sticky="W")
+Label(right_collage_opt_panel, text="Sigma: ").grid(
+    row=3, column=0, sticky="W", pady=(10, 2))
+Entry(right_collage_opt_panel, textvariable=sigma, width=8).grid(
+    row=3, column=1, sticky="W", pady=(10, 2))
+Label(right_collage_opt_panel, text="Color space: ").grid(
+    row=4, column=0, sticky="W")
+OptionMenu(right_collage_opt_panel, color_space, "", *
+           make_img.all_color_spaces).grid(row=4, column=1, sticky="W")
 
-Separator(right_collage_opt_panel, orient="horizontal").grid(row=6, columnspan=2, sticky="we", pady=(5, 5))
+Separator(right_collage_opt_panel, orient="horizontal").grid(
+    row=6, columnspan=2, sticky="we", pady=(5, 5))
 
 ctype = StringVar()
 ctype.set("float16")
@@ -263,16 +282,21 @@ dup.set(1)
 
 collage_even_panel = PanedWindow(right_collage_opt_panel)
 Label(collage_even_panel, text="C Types: ").grid(row=0, column=0, sticky="W")
-OptionMenu(collage_even_panel, ctype, "", *make_img.all_ctypes).grid(row=0, column=1, sticky="W")
-Label(collage_even_panel, text="Duplicates: ").grid(row=1, column=0, sticky="W")
-Entry(collage_even_panel, textvariable=dup, width=5).grid(row=1, column=1, sticky="W")
+OptionMenu(collage_even_panel, ctype, "", *
+           make_img.all_ctypes).grid(row=0, column=1, sticky="W")
+Label(collage_even_panel, text="Duplicates: ").grid(
+    row=1, column=0, sticky="W")
+Entry(collage_even_panel, textvariable=dup,
+      width=5).grid(row=1, column=1, sticky="W")
 collage_even_panel.grid(row=7, columnspan=2, sticky="W")
 
 max_width = IntVar()
 max_width.set(50)
 collage_uneven_panel = PanedWindow(right_collage_opt_panel)
-Label(collage_uneven_panel, text="Max width: ").grid(row=0, column=0, sticky="W")
-Entry(collage_uneven_panel, textvariable=max_width, width=5).grid(row=0, column=1, sticky="W")
+Label(collage_uneven_panel, text="Max width: ").grid(
+    row=0, column=0, sticky="W")
+Entry(collage_uneven_panel, textvariable=max_width,
+      width=5).grid(row=0, column=1, sticky="W")
 
 
 def attach_even():
@@ -300,23 +324,39 @@ def generate_collage():
     if not os.path.isfile(dest_img_path.get()):
         return messagebox.showerror("No destination image", "Please first load the image that you're trying to fit")
     else:
-        if even.get() == "even":
-            def action():
-                result_grid, sorted_imgs, cost = make_img.calculate_collage_bipartite(dest_img_path.get(), imgs,
-                                                                                      dup.get(), color_space.get(),
-                                                                                      ctype.get(), float(sigma.get()),
-                                                                                      out_wrapper)
-                return make_img.make_collage(result_grid, sorted_imgs, False, out_wrapper)
-        else:
-            def action():
-                result_grid, sorted_imgs, cost = make_img.calculate_collage_dup(dest_img_path.get(), imgs,
-                                                                                max_width.get(),
-                                                                                color_space.get(),
-                                                                                float(sigma.get()), out_wrapper)
-                return make_img.make_collage(result_grid, sorted_imgs, False, out_wrapper)
+        try:
+            assert os.path.isfile(
+                dest_img_path.get()), "Destination image does not exist!"
 
-        pool = ThreadPool(1)
-        pool.apply_async(action, args=(), callback=show_img)
+            if even.get() == "even":
+                assert dup.get() > 0, "Duplication must be a positive number"
+                assert float(sigma.get()) != 0, "Sigma must be non-zero"
+
+                def action():
+                    try:
+                        result_grid, sorted_imgs, cost = make_img.calculate_collage_bipartite(dest_img_path.get(), imgs,
+                                                                                              dup.get(), color_space.get(),
+                                                                                              ctype.get(), float(sigma.get()),
+                                                                                              out_wrapper)
+                        return make_img.make_collage(result_grid, sorted_imgs, False, out_wrapper)
+                    except:
+                        messagebox.showerror("Error", traceback.format_exc())
+            else:
+                assert max_width.get() > 0, "Max width must be a positive number"
+                def action():
+                    try:
+                        result_grid, sorted_imgs, cost = make_img.calculate_collage_dup(dest_img_path.get(), imgs,
+                                                                                        max_width.get(),
+                                                                                        color_space.get(),
+                                                                                        float(sigma.get()), out_wrapper)
+                        return make_img.make_collage(result_grid, sorted_imgs, False, out_wrapper)
+                    except:
+                        messagebox.showerror("Error", traceback.format_exc())
+
+            pool = ThreadPool(1)
+            pool.apply_async(action, callback=show_img)
+        except:
+            return messagebox.showerror("Error", traceback.format_exc())
 
 
 Button(right_collage_opt_panel, text=" Generate Collage ",
@@ -338,7 +378,8 @@ Radiobutton(right_top_panel, text="Sort", value="sort", variable=opt,
 Radiobutton(right_top_panel, text="Collage", value="collage", variable=opt,
             command=attach_collage).grid(row=4, column=1, sticky="W")
 
-Separator(right_panel, orient="horizontal").grid(row=3, columnspan=2, sticky="we", pady=(0, 10))
+Separator(right_panel, orient="horizontal").grid(
+    row=3, columnspan=2, sticky="we", pady=(0, 10))
 
 
 def save_img():
@@ -353,7 +394,8 @@ def save_img():
             cv2.imwrite(fp, result_img)
 
 
-Button(right_panel, text=" Save image ", command=save_img).grid(row=4, columnspan=2)
+Button(right_panel, text=" Save image ",
+       command=save_img).grid(row=4, columnspan=2)
 
 # make the window appear at the center
 # https://www.reddit.com/r/Python/comments/6m03sh/make_tkinter_window_in_center_of_screen_newbie/
